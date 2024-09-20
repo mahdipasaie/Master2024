7b77c0bca6f0a99f4f4929cc9da3d619faba0504
Mahdi Pasha <51109234+mahdipasaie@users.noreply.github.com>
2024-09-20 13:55:48 -0400

    Add files via upload

--- classpf.py
+++ classpf.py
@@ -1,8 +1,9 @@
 import fenics as fe
+import numpy as np
 
 class ClassPF:
 
-    def __init__(self, params, mesh):
+    def __init__(self, mesh, params, old_sv_=None, old_sv=None):
 
         self.mesh = mesh
         self.params = params
@@ -11,6 +12,17 @@ class ClassPF:
         self.ep4 = params['ep4']
         self.X = fe.SpatialCoordinate(self.mesh)
         self.Y = self.X[1]
+        self.G = params['G']
+        self.V = params['V']
+        self.ml = params['ml']
+        self.c0 = params['c0']
+        self.keq = params['keq']
+        self.lamda = params['lamda']
+        self.ds = params['ds']
+        self.dl = params['dl']
+        self.Wscale = params['Wscale']
+        self.Tscale = params['Tauscale']
+        self.T = params['Time']
         reltol = params['reltol']
         abstol = params['abstol']
         linearsolverpf = params['linearsolverpf']
@@ -22,6 +34,11 @@ class ClassPF:
             'report': False,"preconditioner": preconditionerpf,
             'error_on_nonconvergence': False,'absolute_tolerance': abstol,
             'relative_tolerance': reltol,'maximum_iterations': maximumiterationspf,}}
+
+        self.dy = params['dy']
+        self.y_solid = params['y_solid']
+        self.old_sv_ = old_sv_
+        self.old_sv = old_sv
 
     def func_space(self, degree=2):
 
@@ -54,13 +71,13 @@ class ClassPF:
             fe.lt(fe.sqrt(self.mgphi), self.tol),
             fe.Constant(1-3*self.ep4),
             1-3*self.ep4+ 4*self.ep4*norm_phi_4)
-        wn = self.w0 * an
-        self.dwnx = fe.conditional(fe.lt(fe.sqrt(self.mgphi), self.tol), 0, fe.diff(wn, dpx))
-        self.dwny = fe.conditional(fe.lt(fe.sqrt(self.mgphi), self.tol), 0, fe.diff(wn, dpy))
+        self.wn = self.w0 * an
+        self.dwnx = fe.conditional(fe.lt(fe.sqrt(self.mgphi), self.tol), 0, fe.diff(self.wn, dpx))
+        self.dwny = fe.conditional(fe.lt(fe.sqrt(self.mgphi), self.tol), 0, fe.diff(self.wn, dpy))
 
     def form(self):
         
-        term0 = (self.G* self.W_scale)*(self.Y-self.V*(self.T*self.Tscale/self.Wscale))/(self.ml* self.c0/self.keq*(1-self.keq))
+        term0 = (self.G* self.Wscale)*(self.Y-self.V*(self.T*self.Tscale/self.Wscale))/(self.ml* self.c0/self.keq*(1-self.keq))
         term4in = self.mgphi*self.wn*self.dwnx
         term5in = self.mgphi*self.wn*self.dwny
         term4 = -fe.inner(term4in, self.v_phi.dx(0)) 
@@ -68,23 +85,25 @@ class ClassPF:
         term3 = -(self.wn**2*fe.inner(fe.grad(self.phi),fe.grad(self.v_phi)))
         term2 = fe.inner((self.phi - self.phi**3)-self.lamda*(self.c + term0)*(1-self.phi**2)**2, self.v_phi) 
         self.taun = (self.wn/self.w0)**2 
-        term1 = -fe.inner((self.taun) * (self.phi-self.phi_) / self.dt, self.v_phi) * fe.dx
+        term1 = -fe.inner((self.taun) * (self.phi-self.phi_) / self.dt, self.v_phi) 
         self.eq1 = term1+term2+term3+term4+term5
+        self.eq1 = self.eq1*fe.dx 
         # second equation
         opk, omk= 1+self.keq, 1-self.keq 
         d = self.ds*(1+self.phi)/2+self.dl*(1-self.phi)/2
         dphidt = (self.phi-self.phi_)/self.dt
-        term6 = -fe.inner(((opk) / 2 - (omk) * self.phi / 2) * (self.c - self.c_) / self.dt, self.v_c) * fe.dx
-        term7 = -fe.inner(d * (1 - self.phi) / 2 * fe.grad(self.c), fe.grad(self.v_c)) * fe.dx
-        term9 = (1 + (omk) * self.c) * dphidt / 2 * self.v_c * fe.dx
+        term6 = -fe.inner(((opk) / 2 - (omk) * self.phi / 2) * (self.c - self.c_) / self.dt, self.v_c) 
+        term7 = -fe.inner(d * (1 - self.phi) / 2 * fe.grad(self.c), fe.grad(self.v_c)) 
+        term9 = (1 + (omk) * self.c) * dphidt / 2 * self.v_c 
         self.eq2 = term6 + term7  + term9 
+        self.eq2 = self.eq2*fe.dx
 
     def defsol(self):
 
         L = self.eq1 + self.eq2  
-        J = fe.derivative(L, self.sol) 
+        J = fe.derivative(L, self.sv) 
         problem = fe.NonlinearVariationalProblem(L, self.sv, J=J) 
-        solverpf = fe.NonlinearVariationalSolver(problem)
+        self.solverpf = fe.NonlinearVariationalSolver(problem)
         self.solverpf.parameters.update(self.solver_parameters)
 
     def solve(self):
@@ -92,3 +111,49 @@ class ClassPF:
         self.solverpf.solve()
         self.sv_.vector()[:]= self.sv.vector()  
 
+    def inco(self):
+
+        class InitialConditions(fe.UserExpression):
+            def __init__(self, dy, y_solid, **kwargs):
+                super().__init__(**kwargs)
+                self.dy = dy
+                self.y_solid = y_solid
+            def eval(self, values, x):
+                xp = x[0]
+                yp = x[1]
+                perturbation_amplitude = 1*self.dy
+                perturbation_wavelength = 4*self.dy  
+                perturbation = perturbation_amplitude*np.sin(2 *np.pi*xp/perturbation_wavelength)
+                if yp < self.y_solid - perturbation_amplitude :
+                    values[0] = 1
+                    values[1] = -1
+                elif self.y_solid - perturbation_amplitude  <= yp <= self.y_solid + perturbation_amplitude: 
+                    values[0] = perturbation 
+                    values[1] = -1
+                else:  # liquid
+                    values[0] = -1
+                    values[1] = -1
+            def value_shape(self):
+                return (2,)
+    
+        if self.old_sv_ is not None:
+
+            fe.LagrangeInterpolator.interpolate(self.sv_, self.old_sv_)
+            fe.LagrangeInterpolator.interpolate(self.sv, self.old_sv)
+
+        else:
+            self.sv_.interpolate(InitialConditions(self.dy, self.y_solid, degree=2))
+            self.sv.interpolate(InitialConditions(self.dy, self.y_solid, degree=2))
+
+    def initilize(self):
+
+        self.func_space()
+        self.inco()
+        self.depvar()
+        self.form()
+        self.defsol()
+
+
+
+
+        
